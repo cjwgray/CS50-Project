b07d6b0ff7ecd596c46194a26daf3721d417d755
cjwgray <126001012+cjwgray@users.noreply.github.com>
2023-07-10 15:31:47 -0500

    first commit

---
+++ project/README.md
@@ -0,0 +1,44 @@
+# Clarence Gaming Den
+#### Video Demo:  <https://youtu.be/9nuFuUCbCzg>
+#### Description:
+
+My project is a web base application name ClarencGamingDen that uses Python, JavaScript, HTML, CSS, and SQL.
+ClarenceGamingDen is a Web site that I reviewed games that I play this year on my PC. After I review the games
+I played, I then post them on my site, write the review, and I scored them. Once the user read all of my reviews for
+each games, they can click the quiz link to answer a question. The user will have to guess which game is my favorite.
+The question will give the user a right or wrong answer. Once the user is done, he or she can click on the comment link
+to leave their thoughts on each of the games that I have reviewed.
+
+Here's a list of the files, templates, and the css styles that I have used.
+
+The files:
+
+* The app.py is where I used all of the python code to have my web base appilcation to be functional. Also, this is where the SQL operates at and stores all the information to the database.
+* The Project.db is where the user names, passwords, and the user comments are stored.
+* The flask_session is where all the flask files are stored.
+
+The Templates folder:
+
+* The apology.html is to display all the apologies for the user to see.
+* The comment.html is where the user can post their comments about the games that was reviewed. This will display the user comments and the time stamp.
+* The favorite.html is where question is located at. Here is where I use the JavaScript to give an anwser to the user when they make a selection.
+* The forspoken.html is where the game Forspoken logo and my review is located at.
+* The sw.html is where the game Star Wars: Surviver logo and my review is located at.
+* The hogwart.html is where the game Hogwart Lagacy logo and my review is located at.
+* The index.html is my home page.
+* The layout.html is where my title and the nav bar is located at.
+* The login.html is where the user login is displayed at.
+* The register.html is where the user register is displayed at.
+
+The static folder contain these files:
+
+* forspoken.webp - this contain the photo of the game forspoken.
+* gamepad.png - this contain the photo of the gamepad that is displayed in the browser.
+* hogwart.webp - this contain the photo of the game Hogwart Lagacy.
+* mgames.webp - this contain the photo of many games that is displayed on my home page.
+* sw.jpg - this contain the photo of the game star wars: Surviver.
+
+The styles.css folder contain all of the designs for the HTML files.
+
+
+
---
+++ project/app.py
@@ -0,0 +1,154 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///Project.db")
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+
+
+@app.route("/")
+@login_required
+def index():
+    """Home page"""
+    return render_template("index.html")
+
+
+@app.route("/forspoken")
+@login_required
+def forspoken():
+    """The Forspoke game page"""
+    return render_template("forspoken.html")
+
+
+@app.route("/hogwart")
+@login_required
+def hogwart():
+    """The Hogwart game page"""
+    return render_template("hogwart.html")
+
+@app.route("/sw")
+@login_required
+def sw():
+    """The Starwars: Surviver game page"""
+    return render_template("sw.html")
+
+@app.route("/favorite")
+@login_required
+def favorite():
+    """This page is for the quiz"""
+    return render_template("favorite.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if (request.method == "POST"):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+
+        if not username:
+            return apology('Please enter username.')
+        elif not password:
+            return apology('Please enter password.')
+        elif not confirmation:
+            return apology('Please confirm password.')
+        if password != confirmation:
+            return apology('Passwords do not match')
+
+        hash = generate_password_hash(password)
+
+        try:
+            db.execute("INSERT INTO users(username, hash) VALUES (?, ?)", username, hash)
+            return redirect('/')
+        except:
+            return apology('Username is already used.')
+    else:
+        return render_template("register.html")
+
+
+@app.route("/comment", methods =["GET", "POST"])
+@login_required
+def comment():
+    """This is for the users to leave comments"""
+    if request.method == "GET":
+        
+        comments = db.execute("SELECT send as comment, date, id FROM comments ORDER BY date DESC")
+        return render_template("comment.html", comments=comments)
+    else:
+        comment = request.form.get("comment")
+
+
+        if not comment:
+            return apology("No Empty Fields")
+
+        db.execute("INSERT INTO comments (send) VALUES (?)", comment)
+        comments = db.execute("SELECT send as comment, date, id FROM comments ORDER BY date DESC")
+
+
+        return render_template("comment.html", comments=comments)
+
+
+
+
+
+
+
